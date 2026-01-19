af7ba755c25fba57515fc8aab6b22a58d7bb2814
Tishetso <tishetsomphelane@gmail.com>
2026-01-19 20:42:08 +0200

    Configuring the marketplace, user is able to see the available listings

--- src/main/java/com/agriconnect/backend/service/ListingService.java
+++ src/main/java/com/agriconnect/backend/service/ListingService.java
@@ -1,6 +1,7 @@
 package com.agriconnect.backend.service;
 
 
+import com.agriconnect.backend.dto.MarketplaceDTO;
 import com.agriconnect.backend.model.Listing;
 import com.agriconnect.backend.repository.ListingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @Transactional
 public class ListingService {
@@ -36,6 +39,28 @@ public class ListingService {
     @Transactional(readOnly = true)
     public List<Listing> findByUserEmail(String email){
         return repo.findByUserEmail(email);
+    }
+
+    //get Available listings for marketplace
+    public List<MarketplaceDTO> getAllAvailableListings(){
+        List <Listing> listings = repo.findAllAvailableListingsWithUser();
+
+        return listings.stream().map(listing -> {
+            String farmerName = listing.getUser().getName() + " " + listing.getUser().getSurname();
+            String location = listing.getUser().getRegion();
+
+            return new MarketplaceDTO(
+                    listing.getId(),
+                    listing.getProduct(),
+                    listing.getQuantity(),
+                    listing.getPrice(),
+                    listing.getImageUrls(),
+                    listing.getStatus(),
+                    farmerName,
+                    location
+            );
+
+        }).collect(Collectors.toList());
     }
 
 }
